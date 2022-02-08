aef6af12035fc2e31805ee0eb09590dee5d14014
pengfei.liu <pengfei.liu@vcg.com>
2022-02-08 17:41:28 +0800

    first commit

---
+++ config/routes.ts
@@ -0,0 +1,77 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
+        ],
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/assets',
+    name: 'assets',
+    icon: 'folder',
+    redirect: '/assets/images',
+    routes: [
+      {
+        path: '/assets/images',
+        name: 'images',
+        icon: 'image',
+        component: './assets/ImagesManage',
+      },
+    ],
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    path: '/new',
+    component: './NewPage',
+    name: 'test',
+  },
+  {
+    component: './404',
+  },
+];
